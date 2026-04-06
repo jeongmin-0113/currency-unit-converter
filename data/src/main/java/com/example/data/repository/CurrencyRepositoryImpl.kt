0a68ee18c50a9a7f551deea5f5a1937cc2c3f198
Jeongmin Park <60566546+jeongmin-0113@users.noreply.github.com>
2026-04-06 18:33:14 +0900

    feat: 환율 리포지터리 구현

---
+++ data/src/main/java/com/example/data/repository/CurrencyRepositoryImpl.kt
@@ -0,0 +1,71 @@
+package com.example.data.repository
+
+import com.example.data.local.ConversionResultDao
+import com.example.data.local.ConversionResultEntity
+import com.example.data.local.CurrencyDao
+import com.example.data.local.CurrencyEntity
+import com.example.data.remote.ExchangeRateApi
+import com.example.domain.model.ConversionResult
+import com.example.domain.model.Currency
+import com.example.domain.repository.CurrencyRepository
+import javax.inject.Inject
+
+class CurrencyRepositoryImpl @Inject constructor(
+    val currencyDao: CurrencyDao,
+    val conversionResultDao: ConversionResultDao,
+    val api: ExchangeRateApi
+) : CurrencyRepository {
+    override suspend fun updateCurrency(): List<Currency> {
+        val apiKey = "api key" // todo: BuildConfig로 분리
+        val response = api.getExchangeRate(apiKey)
+
+        val entities = response.rates.map { (code, rate) ->
+            CurrencyEntity(
+                code = code,
+                name = code,
+                rate = rate
+            )
+        }
+        currencyDao.saveCurrency(entities)
+
+        return entities.map { entity ->
+            Currency(
+                code = entity.code,
+                name = entity.name,
+                rate = entity.rate
+            )
+        }
+    }
+
+    override suspend fun getCurrency(code: String): Currency? {
+        return currencyDao.getCurrency(code)?.let {
+            Currency(it.code, it.name, it.rate)
+        }
+    }
+
+    override suspend fun saveConversionResult(result: ConversionResult) {
+        val entity = ConversionResultEntity(
+            fromCode = result.fromCode,
+            fromName = result.fromName,
+            fromAmount = result.fromAmount,
+            toCode = result.toCode,
+            toName = result.toName,
+            toAmount = result.toAmount
+        )
+
+        conversionResultDao.saveConversionResult(entity)
+    }
+
+    override suspend fun getAllConversionResults(): List<ConversionResult> {
+        return conversionResultDao.getAllConversionResults().map { entity ->
+            ConversionResult(
+                fromCode = entity.fromCode,
+                fromName = entity.fromName,
+                fromAmount = entity.fromAmount,
+                toCode = entity.toCode,
+                toName = entity.toName,
+                toAmount = entity.toAmount
+            )
+        }
+    }
+}
